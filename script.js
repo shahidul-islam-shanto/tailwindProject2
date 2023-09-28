0e29dcce6de6c99fd80b8ab0005c867be8d307de
Shahidulis <shahidulislam47326@gmail.com>
2023-09-29 01:30:14 +0600

    ready

---
+++ script.js
@@ -0,0 +1,6 @@
+let mobileIcon = document.getElementById("mobile-icon");
+let mainMenu = document.getElementById("main-menu");
+
+mobileIcon.addEventListener("click", ()=>{
+   mainMenu.classList.toggle('hidden');
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    
+    extend: {
+      screens:{
+        xsm: '430px',
+        ysm: '524px',
+        md: '640px',
+        lg: '768px',
+        xl: '1024px',
+
+    },
+     },
   },
   plugins: [],
 }
