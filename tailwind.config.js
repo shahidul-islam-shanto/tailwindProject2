9a8df582965826984bfe0ed17564411e9736f5da
Shahidulis <shahidulislam47326@gmail.com>
2023-10-07 00:39:26 +0600

    yesh

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,7 @@ module.exports = {
   theme: {
     
     extend: {
-      screens:{
-        xsm: '430px',
-        ysm: '524px',
-        md: '640px',
-        lg: '768px',
-        xl: '1024px',
-
-    },
+      
      },
   },
   plugins: [],
